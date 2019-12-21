b0ef35b1767c9f9a1d08e699a1df08e9025e4b60
ChenJingZhuo <3279838729@qq.com>
2019-12-21 16:02:25 +0800

    2.添加一些说明

--- app/src/main/java/com/cjz/gson/MainActivity.java
+++ app/src/main/java/com/cjz/gson/MainActivity.java
@@ -41,15 +41,16 @@ import interfaces.heweather.com.interfacesmodule.view.HeWeather;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private String weatherCountry = "http://guolin.tech/api/china/";
+    private String weatherCountry = "http://guolin.tech/api/china/";//中国的省份城市api
     private String weatherProvince, weatherCity;
     private ArrayList<Province> provinceList;
     private ArrayList<City> cityList;
-    //    private String weatherApi = "http://guolin.tech/api/weather?cityid=CN101281701&key=00d6c3f6c9994cc1ae92d054f77828c0";//完整请求路径(例子)
-    private String weatherUrl = "http://guolin.tech/api/weather?cityid=%s&key=%s";
-    private String weatherUrl2 = "https://free-api.heweather.net/s6/weather/now?location=%s&key=%s";
-    private String cityId;
 
+    private String weatherUrl = "https://free-api.heweather.net/s6/weather/now?location=%s&key=%s";//和风天气 免费的api接口
+    private String cityId;//具体城市天气id，如湛江，"weather_id"=CN101281001
+
+    //注册和风天气开发者https://dev.heweather.com/
+    //创建的apk 的 用户id 以及 该apk的key（包名要一致）
     private String userName = "HE1912210205271939";
     private String key = "3fc1fce8eac14383b1ba8a408bf59c40";//自己申请的key
 
@@ -84,87 +85,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+//        使用 SDK 时，需提前进行账户初始化（全局执行一次即可）
         HeConfig.init(userName, key);
+//        个人开发者、企业开发者、普通用户等所有使用免费数据的用户需要切换到免费服务域名 即 https://free-api.heweather.net/s6/sdk/
         HeConfig.switchToFreeServerNode();
-    }
-
-    public void queryWeather2(){
-        /**
-         * 实况天气
-         * 实况天气即为当前时间点的天气状况以及温湿风压等气象指数，具体包含的数据：体感温度、
-         * 实测温度、天气状况、风力、风速、风向、相对湿度、大气压强、降水量、能见度等。
-         *
-         * @param context  上下文
-         * @param location 地址详解
-         * @param lang     多语言，默认为简体中文，海外城市默认为英文
-         * @param unit     单位选择，公制（m）或英制（i），默认为公制单位
-         * @param listener 网络访问回调接口
-         */
-        HeWeather.getWeatherNow(MainActivity.this, cityId, Lang.CHINESE_SIMPLIFIED , Unit.METRIC , new HeWeather.OnResultWeatherNowBeanListener() {
-            @Override
-            public void onError(Throwable e) {
-                Log.i(TAG, "Weather Now onError: ", e);
-            }
-
-            @Override
-            public void onSuccess(Now dataObject) {
-                Log.i(TAG, " Weather Now onSuccess: " + new Gson().toJson(dataObject));
-                //先判断返回的status是否正确，当status正确时获取数据，若status不正确，可查看status对应的Code值找到原因
-                if ( Code.OK.getCode().equalsIgnoreCase(dataObject.getStatus()) ){
-                    //此时返回数据
-
-                    Basic basic=dataObject.getBasic();
-                    String location=basic.getLocation();
-
-                    mCityWeather.setText(location+"的天气");
-
-                    NowBase now = dataObject.getNow();
-
-                    String tmp=now.getTmp();
-                    String cond_txt=now.getCond_txt();
-                    String wind_dir=now.getWind_dir();
-
-                    mWeather.setText("当前温度："+tmp+"℃，"+cond_txt+"，"+wind_dir);
-                    String hum=now.getHum();
-                    mShidu.setText(hum+"%");
-                } else {
-                    //在此查看返回数据失败的原因
-                    String status = dataObject.getStatus();
-                    Code code = Code.toEnum(status);
-                    Log.i(TAG, "failed code: " + code);
-                }
-            }
-        });
-
-        HeWeather.getWeatherLifeStyle(MainActivity.this,cityId, new HeWeather.OnResultWeatherLifeStyleBeanListener() {
-            @Override
-            public void onError(Throwable throwable) {
-
-            }
-
-            @Override
-            public void onSuccess(Lifestyle lifestyle) {
-                List<LifestyleBase> lifestyleBases=lifestyle.getLifestyle();
-                String shushidu=lifestyleBases.get(0).getBrf();//舒适度指数
-                String shushidu2=lifestyleBases.get(0).getTxt();//舒适度建议
-                String sport=lifestyleBases.get(3).getBrf();//运动指数
-                String sport2=lifestyleBases.get(3).getTxt();//运动建议
-                String cw=lifestyleBases.get(6).getBrf();//洗车指数
-                String cw2=lifestyleBases.get(6).getTxt();//洗车建议
-
-                mSuggestion.setText("舒适度指数："+shushidu+"\n" +
-                        "舒适度建议："+shushidu2+"\n" +
-                                "运动指数："+sport+"\n" +
-                                "运动建议："+sport2+"\n" +
-                                "洗车指数："+cw+"\n" +
-                                "洗车建议："+cw2+"\n");
-
-                Message message=new Message();
-                message.what=1;
-                MainActivity.this.myHandler.sendMessage(message);
-            }
-        });
-
     }
 
     private void initView() {
@@ -192,21 +116,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
-
-    Handler myHandler = new Handler(){
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            switch (msg.what){
-                case 1:
-                    mCityWeather.setVisibility(View.VISIBLE);
-                    mWeatherInfo.setVisibility(View.VISIBLE);
-                    weatherProvince = "";
-                    weatherCity = "";
-                    break;
-            }
-            super.handleMessage(msg);
-        }
-    };
 
     private void queryWeather() {
         provinceList = new ArrayList<Province>();//省份集合
@@ -335,7 +244,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     cityId=cityIdInfo.getString("weather_id");
 
                     //拼接字符串
-                    String weatherApi = String.format(weatherUrl2, cityId, key);
+                    String weatherApi = String.format(weatherUrl, cityId, key);
 
                     Log.d("WeatherApi", "" + weatherApi);
 
@@ -416,5 +325,99 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }.start();
     }
+
+    public void queryWeather2(){
+        /**
+         * 实况天气
+         * 实况天气即为当前时间点的天气状况以及温湿风压等气象指数，具体包含的数据：体感温度、
+         * 实测温度、天气状况、风力、风速、风向、相对湿度、大气压强、降水量、能见度等。
+         *
+         * @param context  上下文
+         * @param location 地址详解
+         * @param lang     多语言，默认为简体中文，海外城市默认为英文
+         * @param unit     单位选择，公制（m）或英制（i），默认为公制单位
+         * @param listener 网络访问回调接口
+         */
+        HeWeather.getWeatherNow(MainActivity.this, cityId, Lang.CHINESE_SIMPLIFIED , Unit.METRIC , new HeWeather.OnResultWeatherNowBeanListener() {
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "Weather Now onError: ", e);
+            }
+
+            @Override
+            public void onSuccess(Now dataObject) {
+                Log.i(TAG, " Weather Now onSuccess: " + new Gson().toJson(dataObject));
+                //先判断返回的status是否正确，当status正确时获取数据，若status不正确，可查看status对应的Code值找到原因
+                if ( Code.OK.getCode().equalsIgnoreCase(dataObject.getStatus()) ){
+                    //此时返回数据
+
+                    Basic basic=dataObject.getBasic();
+                    String location=basic.getLocation();
+
+                    mCityWeather.setText(location+"的天气");
+
+                    NowBase now = dataObject.getNow();
+
+                    String tmp=now.getTmp();
+                    String cond_txt=now.getCond_txt();
+                    String wind_dir=now.getWind_dir();
+
+                    mWeather.setText("当前温度："+tmp+"℃，"+cond_txt+"，"+wind_dir);
+                    String hum=now.getHum();
+                    mShidu.setText(hum+"%");
+                } else {
+                    //在此查看返回数据失败的原因
+                    String status = dataObject.getStatus();
+                    Code code = Code.toEnum(status);
+                    Log.i(TAG, "failed code: " + code);
+                }
+            }
+        });
+
+        HeWeather.getWeatherLifeStyle(MainActivity.this,cityId, new HeWeather.OnResultWeatherLifeStyleBeanListener() {
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onSuccess(Lifestyle lifestyle) {
+                List<LifestyleBase> lifestyleBases=lifestyle.getLifestyle();
+                String shushidu=lifestyleBases.get(0).getBrf();//舒适度指数
+                String shushidu2=lifestyleBases.get(0).getTxt();//舒适度建议
+                String sport=lifestyleBases.get(3).getBrf();//运动指数
+                String sport2=lifestyleBases.get(3).getTxt();//运动建议
+                String cw=lifestyleBases.get(6).getBrf();//洗车指数
+                String cw2=lifestyleBases.get(6).getTxt();//洗车建议
+
+                mSuggestion.setText("舒适度指数："+shushidu+"\n" +
+                        "舒适度建议："+shushidu2+"\n" +
+                        "运动指数："+sport+"\n" +
+                        "运动建议："+sport2+"\n" +
+                        "洗车指数："+cw+"\n" +
+                        "洗车建议："+cw2+"\n");
+
+                Message message=new Message();
+                message.what=1;
+                MainActivity.this.myHandler.sendMessage(message);
+            }
+        });
+
+    }
+
+    Handler myHandler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what){
+                case 1:
+                    mCityWeather.setVisibility(View.VISIBLE);
+                    mWeatherInfo.setVisibility(View.VISIBLE);
+                    weatherProvince = "";
+                    weatherCity = "";
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
 
 }
